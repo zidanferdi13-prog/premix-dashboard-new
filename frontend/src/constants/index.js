151904e86e7e9e42e3b6f103a21e83f7d2a4d36d
Zidan Ferdiansyah <zidanferdi13@gmail.com>
2026-06-19 09:07:30 +0700

    kondisi state

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -3,4 +3,4 @@
 export const APP_NAME = 'WAN Factory Premix'
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://services.ama.id'
-export const API_PRINT_URL = import.meta.env.VITE_API_PRINT_URL || 'http://localhost:3000/print'
+export const API_PRINT_URL = import.meta.env.VITE_API_PRINT_URL || 'http://localhost:3001/print'
